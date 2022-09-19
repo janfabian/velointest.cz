ae51e1dd057f0a34000b2b2f50b28868d68e0291
janfabian <mail@janfabian.cz>
2022-09-19 16:00:05 +0200

    favicons

--- static/custom.js
+++ static/custom.js
@@ -21,37 +21,36 @@
               }
         });
 
-
-        // Contact Form
-        $("#contact").submit(function (e) {
-          e.preventDefault();
-          var name = $("#cf-name").val();
-          var email = $("#cf-email").val();
-          var subject = $("#cf-subject").val();
-          var message = $("#cf-message").val();
-          var dataString = 'name=' + name + '&email=' + email + '&subject=' + subject + '&message=' + message;
-
-          function isValidEmail(emailAddress) {
-              var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-              return pattern.test(emailAddress);
-          };
-          if (isValidEmail(email) && (message.length > 1) && (name.length > 1)) {
-              $.ajax({
-                  type: "POST",
-                  url: "php/contact.php",
-                  data: dataString,
-                  success: function () {
-                      $('.text-success').fadeIn(1000);
-                      $('.text-danger').fadeOut(500);
-                  }
-              });
-          }
-          else {
-              $('.text-danger').fadeIn(1000);
-              $('.text-success').fadeOut(500);
-          }
-          return false;
-        });
+        var form = document.getElementById("contact-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.innerHTML = "Vaše zpráva je úspěšně odeslaná. Děkujeme";
+          form.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "Omlouváme se, nastal problém při odesílání vaší zprávy. Napište prosím na velointest@velointest.cz nebo zavolejte na 777 271 896."
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Omlouváme se, nastal problém při odesílání vaší zprávy. Napište prosím na velointest@velointest.cz nebo zavolejte na 777 271 896."
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
 
 
         // Owl Carousel
